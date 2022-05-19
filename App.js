19c4ad86d6acc72f0398fbbcc9a7a749da6597af
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-19 20:54:03 +0100

    Feat: Added Login Screen interface

--- App.js
+++ App.js
@@ -1,32 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./src/screen/Login";
 
 const Stack = createNativeStackNavigator();
-
-const Login = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={Login}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/constants/randomDataIcons.js
@@ -0,0 +1,27 @@
+const names = [
+  "insert-emoticon",
+  "queue-music",
+  "all-inclusive",
+  "music-video",
+  "library-music",
+  "control-point-duplicate",
+  "music-note",
+  "emoji-people",
+  "ev-station",
+  "event-seat",
+  "festival",
+  "gesture",
+];
+
+const colors = [
+  "#007D74",
+  "#A569BD",
+  "#CD6155",
+  "#2980B9",
+  "#D4AC0D",
+  "#AA5A25",
+  "#677172",
+  "#34495E",
+];
+
+export default { names, colors };
