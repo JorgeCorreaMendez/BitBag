aef24a8afce2ddf8fff3f6d5b92179f813cbda17
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-19 16:37:37 +0100

    style: add color and size constants

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export default {
+  primary: "#FFF",
+  secundary: "#878787",
+  superficies: "#344955",
+  background: "#232F34",
+  error: "#BB0000",
+  selected: "#557385",
+  success: "#038200",
+};
---
+++ src/constants/size.js
@@ -0,0 +1,7 @@
+export default {
+  h1: 30,
+  h2: 24,
+  h3: 20,
+  h4: 17,
+  small: 10,
+};
