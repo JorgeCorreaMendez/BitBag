b1908cffe5043771b8842903ba40a53a0293b15f
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-19 21:43:50 +0100

    Feat: Added a modal alert component

---
+++ src/components/modal/ModalAlert.jsx
@@ -0,0 +1,43 @@
+import { Modal, Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import size from "../../constants/size";
+import colors from "../../constants/colors";
+
+const ModalAlert = ({ visible, closeModal, iconName, text, color }) => {
+  return (
+    <Modal animationType="slice" visible={visible} transparent={true}>
+      <TouchableOpacity onPress={() => closeModal()}>
+        <View style={styles.container}>
+          <View style={[styles.alertContainer, { backgroundColor: color }]}>
+            <Feather name={iconName} size={30} color="white" />
+            <View>
+              <Text style={styles.textTitle}>{text}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    paddingVertical: "20%",
+    paddingHorizontal: "5%",
+    justifyContent: "flex-end",
+  },
+  alertContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: "6%",
+    borderRadius: 5,
+  },
+  textTitle: {
+    color: colors.primary,
+    fontSize: size.h3,
+    paddingLeft: 20,
+  },
+});
+
+export default ModalAlert;
