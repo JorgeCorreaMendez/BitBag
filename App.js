11ae536c2093a4cc81588482258303792c32c6fa
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-21 12:44:41 +0100

    Style: added to header of Tracks screen

--- App.js
+++ App.js
@@ -3,18 +3,35 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./src/screen/Login";
 import Tracks from "./src/screen/Tracks";
 
+import colors from "./src/constants/colors";
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.background,
+          },
+          headerTintColor: colors.primary,
+          headerTitleAlign: "center",
+        }}
+      >
         <Stack.Screen
           name="Login"
           options={{ headerShown: false }}
           component={Login}
         />
-        <Stack.Screen name="Tracks" component={Tracks} />
+        <Stack.Screen
+          name="Tracks"
+          component={Tracks}
+          options={{
+            title: "Pistas",
+            headerBackVisible: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
