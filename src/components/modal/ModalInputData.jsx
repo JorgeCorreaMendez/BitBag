ef7e165f226b28c0f1aa6c010ae5c4c89578efd5
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-21 19:16:42 +0100

    Feat: Added new ModalInputData component

---
+++ src/components/modal/ModalInputData.jsx
@@ -0,0 +1,94 @@
+import {
+  Modal,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import colors from "../../constants/colors";
+import size from "../../constants/size";
+
+const ModalInputData = ({
+  visible,
+  closeModal,
+  value,
+  onChange,
+  iconName,
+  onPress,
+  ...props
+}) => {
+  return (
+    <Modal animationType="fade" visible={visible} transparent={true}>
+      <View style={styles.background}>
+        <Modal animationType="slice" visible={visible} transparent={true}>
+          <TouchableOpacity
+            onPress={() => closeModal()}
+            style={styles.container}
+          >
+            <View style={styles.modalContainer}>
+              <Text style={styles.textInfo}>
+                Introduzca su correo para poder enviarle un metodo de
+                recoperación.
+              </Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  value={value}
+                  onChangeText={(text) => onChange(text)}
+                  style={styles.textInput}
+                  placeholderTextColor={colors.primary}
+                  {...props}
+                />
+                <TouchableOpacity onPress={() => onPress()}>
+                  <MaterialIcons
+                    name={iconName}
+                    size={30}
+                    color={colors.primary}
+                    style={{ paddingLeft: 10 }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "rgba(0, 0, 0, 0.50)",
+    height: "100%",
+  },
+  container: {
+    paddingVertical: "10%",
+    paddingHorizontal: "3%",
+  },
+  modalContainer: {
+    backgroundColor: colors.background,
+    borderRadius: 15,
+    paddingLeft: 20,
+    paddingVertical: "8%",
+  },
+  textInfo: {
+    color: colors.primary,
+    fontSize: size.h4,
+    paddingBottom: 20,
+  },
+  inputContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  textInput: {
+    width: "80%",
+    fontSize: size.h3,
+    backgroundColor: colors.superficies,
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
+
+export default ModalInputData;
