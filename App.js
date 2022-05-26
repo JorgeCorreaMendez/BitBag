e218644a1f3eeff67793433ab6e376d78ab2e7ef
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-26 11:29:57 +0100

    Fix: no pass for login

--- App.js
+++ App.js
@@ -68,7 +68,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {!user ? (
+      {user ? (
         <Tab.Navigator
           screenOptions={{
             ...commonsOptions,
