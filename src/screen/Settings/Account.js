6ca94deb8af66a13da9c2742019280a152ffe97e
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-05-26 10:39:33 +0100

    Feat: Added Account screen interface

--- src/screen/Settings/Account.js
+++ src/screen/Settings/Account.js
@@ -1,5 +1,43 @@
+import { Text, View, StyleSheet } from "react-native";
+import Button from "../../components/button/MyButton";
+import colors from "../../constants/colors";
+import size from "../../constants/size";
+
 const Account = () => {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <View style={{ paddingVertical: "10%" }}>
+        <Text style={styles.dataTitle}>Correo Electronico</Text>
+        <Text style={styles.emailText}>jorgecorreamen@gmail.com</Text>
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <Button title="Eliminar cuenta" style={styles.deleteUserButton} />
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingHorizontal: 20,
+  },
+  dataTitle: {
+    color: colors.primary,
+    fontSize: size.h2,
+  },
+  emailText: {
+    color: colors.secundary,
+    fontSize: size.h3,
+    textDecorationLine: "underline",
+  },
+  deleteUserButton: {
+    color: colors.primary,
+    backgroundColor: colors.superficies,
+    paddingVertical: 20,
+    paddingHorizontal: "20%",
+  },
+});
 
 export default Account;
